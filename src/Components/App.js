1a40f6810417380ea21c10503796618149cad1f9
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-09-24 20:55:10 -0600

    Add components and files for nav and first pixel art. Begin styling first 5 rows.

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+import Bulbasaur from './Bulbasaur';
+import '../CSS/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Bulbasaur />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import '../CSS/Nav.scss';
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1 className="app-title">Pokemon Pixel Gallery!</h1>
+      <div className="pokemon-btn-div">
+        <button className="nav-bulbasaur btn">Bulbasaur</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
