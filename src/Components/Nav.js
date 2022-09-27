86295ec3beb090e80fba5e473d0fedfc6eed1dd2
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-09-27 14:11:56 -0600

    Add react router to project.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,6 +6,10 @@ const Nav = () => {
       <h1 className="app-title">Pokemon Pixel Gallery!</h1>
       <div className="pokemon-btn-div">
         <button className="nav-bulbasaur btn">Bulbasaur</button>
+        <button className="nav-charmander btn">Charmander</button>
+        <button className="nav-squirtle btn">Squirtle</button>
+        <button className="nav-pikachu btn">Pikachu</button>
+        <button className="nav-lapras btn">Lapras</button>
       </div>
     </nav>
   )
